fd9bffddf5225897c984565b2e1f7edb9ae687aa
Thierry TRAORE <traore.toubathierry@gmail.com>
2024-04-28 19:38:13 +0200

    "maj"

--- src/app/models/ticket.model.ts
+++ src/app/models/ticket.model.ts
@@ -1,4 +1,11 @@
 import { BaseModel } from "./base.model";
+import { TagModel } from "./tag.model";
 
 export class TicketModel extends BaseModel {
+  label?: string;
+  description?: string;
+  status?: string;
+  tags?: TagModel[]
+  openedById?: string;
+  closedById?: string;
 }
--- src/app/pages/ticket/ticket.component.ts
+++ src/app/pages/ticket/ticket.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { TicketService } from "../../services/ticket.service";
+import { TicketModel } from "../../models/ticket.model";
 
 @Component({
   selector: 'app-ticket',
@@ -7,6 +9,23 @@ import { Component } from '@angular/core';
   templateUrl: './ticket.component.html',
   styleUrl: './ticket.component.scss'
 })
-export class TicketComponent {
+export class TicketComponent implements OnInit {
+
+  ticketRows = [] as TicketModel[];
+  ticketService = inject(TicketService);
+
+  ngOnInit(): void {
+    this.fetchTicket();
+  }
+
+  private fetchTicket() {
+    this.ticketService.getAll().subscribe({
+      next: (result) => {
+        this.ticketRows = result;
+        console.log(result);
+      },
+      error: (e) => console.error(e)
+    });
+  }
 
 }
