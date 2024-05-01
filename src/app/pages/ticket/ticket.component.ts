5011039c05d0e77e92dbe891d4729c7f4e2cdaa2
Thierry TRAORE <traore.toubathierry@gmail.com>
2024-05-01 06:58:20 +0200

    "update"

--- src/app/pages/ticket/ticket.component.ts
+++ src/app/pages/ticket/ticket.component.ts
@@ -1,31 +1,102 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { TicketService } from "../../services/ticket.service";
 import { TicketModel } from "../../models/ticket.model";
+import { CommonModule, UpperCasePipe } from "@angular/common";
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { TagService } from "../../services/tag.service";
+import { TagModel } from "../../models/tag.model";
+import { IDropdownSettings, NgMultiSelectDropDownModule } from "ng-multiselect-dropdown";
+import { LocalStorageService } from "../../utilities/local-storage.service";
+import { PersonModel } from "../../models/person.model";
 
 @Component({
   selector: 'app-ticket',
   standalone: true,
-  imports: [],
+  imports: [
+    UpperCasePipe, ReactiveFormsModule, CommonModule, NgMultiSelectDropDownModule, FormsModule,
+  ],
   templateUrl: './ticket.component.html',
   styleUrl: './ticket.component.scss'
 })
 export class TicketComponent implements OnInit {
 
-  ticketRows = [] as TicketModel[];
   ticketService = inject(TicketService);
+  tagService = inject(TagService);
+  localStorageService = inject(LocalStorageService);
+
+  ticketRows = [] as TicketModel[];
+  tagRows = [] as TagModel[];
+  currentTicket: TicketModel = {} as TicketModel;
+
+  createTicketForm = new FormGroup({
+    label: new FormControl(''),
+    description: new FormControl(''),
+    status: new FormControl('')
+  });
+
+  dropdownSettings: IDropdownSettings = {
+    singleSelection: false,
+    itemsShowLimit: 5,
+    allowSearchFilter: true,
+    idField: 'id',
+    textField: 'label',
+  };
+  selectedTags = [] as TagModel;
 
   ngOnInit(): void {
     this.fetchTicket();
+    this.tagService.getAll().subscribe({
+      next: (result) => {
+        this.tagRows = result;
+      },
+      error: (e) => console.error(e)
+    });
+  }
+
+  showModal(index: number | null) {
+    if (index !== null) {
+      this.currentTicket = this.ticketRows[index];
+    }
+  }
+
+  closeModal() {
+    this.currentTicket = {} as TicketModel;
+  }
+
+  deleteTicket() {
+    const id = this.currentTicket.id as string;
+    this.ticketService.delete(id).subscribe({
+      next: (result) => {
+        console.log('suppression effectuée avec succès');
+        this.fetchTicket();
+      },
+      error: (e) => console.error(e)
+    })
+  }
+
+  createTicket() {
+    const person = JSON.parse(<string>this.localStorageService.getItem('loggedUser')) as PersonModel;
+    const ticket = {
+      ...this.createTicketForm,
+      openedById: person.id,
+      closedById: person.id,
+      tags: this.selectedTags as TagModel[]
+    } as TicketModel
+    this.ticketService.create(ticket).subscribe({
+      next: (result) => {
+        console.log('creation effectuée avec succès');
+        this.fetchTicket();
+      },
+      error: (e) => console.error(e)
+    })
   }
 
   private fetchTicket() {
     this.ticketService.getAll().subscribe({
       next: (result) => {
         this.ticketRows = result;
-        console.log(result);
       },
       error: (e) => console.error(e)
     });
   }
-
 }
